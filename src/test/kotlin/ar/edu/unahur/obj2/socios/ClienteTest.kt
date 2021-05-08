9572b0f34bbd734ae67387db7b1c14706997b8d4
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-05-08 15:42:30 -0300

    primer test resfriado y en las lauchas

--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,17 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
+
   describe("Un/a cliente") {
+
+
+    var matias  = Cliente(Resfriado(),1000,LasLauchas(), Propina(Resfriado(),LasLauchas()))
+
+    matias.simularPropina(1000).shouldBe(500)
+
+
   }
 })
