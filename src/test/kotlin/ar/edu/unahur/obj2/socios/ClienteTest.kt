425e665d9c550c9ddbc2248b20b91a33cc180b0c
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-05-09 11:59:29 -0300

    Se completan tests

--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -2,6 +2,7 @@ package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import java.security.acl.LastOwnerException
 
 class ClienteTest : DescribeSpec({
 
@@ -57,12 +58,54 @@ class ClienteTest : DescribeSpec({
 
     }
 
+    describe("Un/a cliente resfriado en las torres") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.RESFRIADO)
+      clienteSantiago.cambiarPlataDelBolsillo(1000)
+      clienteSantiago.cambiarDeBarrio(LasTorres())
+
+      clienteSantiago.propinaMasBarrio(1000).shouldBe(1000)
+
+    }
+
+    describe("Un/a cliente resfriado en barrio verde") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.RESFRIADO)
+      clienteSantiago.cambiarPlataDelBolsillo(1000)
+      clienteSantiago.cambiarDeBarrio(BarrioVerde())
+
+      clienteSantiago.propinaMasBarrio(1000).shouldBe(1200)
+
+    }
+
     describe("Un/a cliente feliz en las rosas") {
       clienteSantiago.cambiarEstadoDeAnimo(Animos.FELIZ)
       clienteSantiago.cambiarPlataDelBolsillo(1000)
       clienteSantiago.cambiarDeBarrio(LasRosas())
 
       clienteSantiago.propinaMasBarrio(1000).shouldBe(300)
+    }
+
+    describe("Un/a cliente feliz en las lauchas") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.FELIZ)
+      clienteSantiago.cambiarPlataDelBolsillo(1000)
+      clienteSantiago.cambiarDeBarrio(LasLauchas())
+
+      clienteSantiago.propinaMasBarrio(1000).shouldBe(125)
+    }
+
+    describe("Un/a cliente feliz en las torres") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.FELIZ)
+      clienteSantiago.cambiarPlataDelBolsillo(1000)
+      clienteSantiago.cambiarDeBarrio(LasTorres())
+
+      clienteSantiago.propinaMasBarrio(1000).shouldBe(250)
+    }
+
+    describe("Un/a cliente feliz en barrio verde") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.FELIZ)
+      clienteSantiago.cambiarPlataDelBolsillo(1000)
+      clienteSantiago.cambiarDeBarrio(BarrioVerde())
+
+      clienteSantiago.propinaMasBarrio(1000).shouldBe(450)
     }
 
     describe("Un/a cliente indiferente en las torres") {
@@ -78,6 +121,36 @@ class ClienteTest : DescribeSpec({
 
     }
 
+    describe("Un/a cliente indiferente en las rosas") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.INDIFERENTE)
+      clienteSantiago.cambiarPlataDelBolsillo(950)
+      clienteSantiago.cambiarDeBarrio(LasRosas())
+
+      clienteSantiago.propinaMasBarrio(75000).shouldBe(1000)
+
+
+    }
+
+    describe("Un/a cliente indiferente en las lauchas") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.INDIFERENTE)
+      clienteSantiago.cambiarPlataDelBolsillo(950)
+      clienteSantiago.cambiarDeBarrio(LasLauchas())
+
+      clienteSantiago.propinaMasBarrio(75000).shouldBe(475)
+
+
+    }
+
+    describe("Un/a cliente indiferente en barrio verde") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.INDIFERENTE)
+      clienteSantiago.cambiarPlataDelBolsillo(950)
+      clienteSantiago.cambiarDeBarrio(BarrioVerde())
+
+      clienteSantiago.propinaMasBarrio(75000).shouldBe(1150)
+
+
+    }
+
     describe("Un/a cliente enojado en barrio verde") {
       clienteSantiago.cambiarEstadoDeAnimo(Animos.ENOJADO)
       clienteSantiago.cambiarPlataDelBolsillo(400)
@@ -85,6 +158,31 @@ class ClienteTest : DescribeSpec({
 
       clienteSantiago.propinaMasBarrio(5460).shouldBe(200)
     }
+
+    describe("Un/a cliente enojado en las rosas") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.ENOJADO)
+      clienteSantiago.cambiarPlataDelBolsillo(400)
+      clienteSantiago.cambiarDeBarrio(LasRosas())
+
+      clienteSantiago.propinaMasBarrio(5460).shouldBe(50)
+    }
+
+    describe("Un/a cliente enojado en las lauchas") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.ENOJADO)
+      clienteSantiago.cambiarPlataDelBolsillo(400)
+      clienteSantiago.cambiarDeBarrio(LasLauchas())
+
+      clienteSantiago.propinaMasBarrio(5460).shouldBe(0)
+    }
+
+    describe("Un/a cliente enojado en las torres") {
+      clienteSantiago.cambiarEstadoDeAnimo(Animos.ENOJADO)
+      clienteSantiago.cambiarPlataDelBolsillo(400)
+      clienteSantiago.cambiarDeBarrio(LasTorres())
+
+      clienteSantiago.propinaMasBarrio(5460).shouldBe(0)
+    }
+
 
   }
 
